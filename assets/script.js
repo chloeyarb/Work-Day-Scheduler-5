84c6a6cb05d25b877e68e4630f4ede72fd6f23de
Chloe Yarborough <chloeyarborough@Chloes-MacBook-Air.local>
2022-02-11 20:13:34 -0500

    added get item for local storage

--- assets/script.js
+++ assets/script.js
@@ -22,19 +22,28 @@ var colorCode = function() {
         } else {
             $(this).addClass("future");
         }
-
-        var tasks = $(this).attr("id");
-        var savedTasks = localStorage.getItem(tasks);
-        $(this).children(".description").val(savedTasks);
     })
 }
 colorCode();
 //Local Storage
-
+//val is how to get value from local storage
+//have to do getItem for each hr
+$("#row-9 textarea").val(localStorage.getItem("row-9"))
+$("#row-10 textarea").val(localStorage.getItem("row-10"))
+$("#row-11 textarea").val(localStorage.getItem("row-11"))
+$("#row-12 textarea").val(localStorage.getItem("row-12"))
+$("#row-13 textarea").val(localStorage.getItem("row-13"))
+$("#row-14 textarea").val(localStorage.getItem("row-14"))
+$("#row-15 textarea").val(localStorage.getItem("row-15"))
+$("#row-16 textarea").val(localStorage.getItem("row-16"))
+$("#row-17 textarea").val(localStorage.getItem("row-17"))
 //eventlisten for onclick save btn to set text in local storage (setItem). Take the value 
 $(".saveBtn").click(function(){
-    var tasks = $(".time-block").attr("id");
-    localStorage.setItem(".time-block", tasks);
+    //$this is whichever save button the click is on - key
+    var key = $(this).parent().attr("id");
+    //will take the value of the text area
+    var task = $(this).siblings("textarea").val()
+    localStorage.setItem(key, task);
 })
 
 
